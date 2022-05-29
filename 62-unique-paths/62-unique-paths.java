43491b2f7ea33b83aaa53af38690307657b40050
Shameek Kumar Baranwal <39854726+shameekbaranwal@users.noreply.github.com>
2022-05-29 21:54:09 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (10.45%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -11,13 +11,18 @@ class Solution {
         
         if(grid[m][n] > 0)
             return grid[m][n];
+        if(grid[n][m] > 0)
+            return grid[n][m];
         
         grid[m][n] = gridTraveller(m-1, n) + gridTraveller(m, n-1);
+        grid[n][m] = grid[m][n];
+        
         return grid[m][n];
     }
     
     public int uniquePaths(int m, int n) {
-        grid = new int[m+1][n+1];
+        int size = Math.max(m,n) + 1;
+        grid = new int[size][size];
    
 		return gridTraveller(m, n);
     }
