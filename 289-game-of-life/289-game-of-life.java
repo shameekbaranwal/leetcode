3cd4b09059c69fca19f93ee84df607910a54ab6d
Shameek <39854726+shameekbaranwal@users.noreply.github.com>
2022-06-22 18:24:21 +0530

    Time: 0 ms (100.00%), Space: 42 MB (72.77%) - LeetHub

--- 289-game-of-life/289-game-of-life.java
+++ 289-game-of-life/289-game-of-life.java
@@ -17,26 +17,18 @@ class Solution {
 
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < columns; j++) {
-				int numOfNeighboursAlive = 0;
+				int neighboursAlive = 0;
 				for (int k = 0; k < 8; k++) {
 					int newi = i + neighbours[k][0];
 					int newj = j + neighbours[k][1];
-					if (newi >= 0 && newi < rows && newj >= 0 && newj < columns) {
-						if (changed[newi][newj])
-							numOfNeighboursAlive += (1 - board[newi][newj]);
-						else
-							numOfNeighboursAlive += board[newi][newj];
-					}
+					if (newi >= 0 && newi < rows && newj >= 0 && newj < columns)
+						neighboursAlive += changed[newi][newj] ? 1 - board[newi][newj] : board[newi][newj];
 				}
-				if (board[i][j] == 0 && numOfNeighboursAlive == 3) {
-					board[i][j] = 1;
+				if ((board[i][j] == 0 && neighboursAlive == 3) ||
+						(board[i][j] == 1 &&
+								(neighboursAlive < 2 || neighboursAlive > 3))) {
+					board[i][j] = 1 - board[i][j];
 					changed[i][j] = true;
-				}
-				if (board[i][j] == 1) {
-					if (numOfNeighboursAlive < 2 || numOfNeighboursAlive > 3) {
-						board[i][j] = 0;
-						changed[i][j] = true;
-					}
 				}
 			}
 		}
