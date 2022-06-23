16df796def1035e8bdd749df2021ce0240210f81
Shameek <39854726+shameekbaranwal@users.noreply.github.com>
2022-06-23 13:16:04 +0530

    Time: 2 ms (90.73%), Space: 68 MB (49.76%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,22 +1,15 @@
 class Solution {
     boolean[] dp;
     public boolean canJump(int[] nums) {
-        dp = new boolean[nums.length];
-        // the array contains whether or not it is possible to 
-        // reach the last index from that position
-        // filling it from the right would make the whole op
-        // run in linear time.
-        dp[nums.length - 1] = true;
-        for(int i = nums.length - 2; i >= 0; i--) {
-            if (nums[i] == 0)
-                dp[i] = false;
-            int max = Math.min(nums[i], nums.length - i - 1);
-            for(int j = max; j > 0; j--) {
-                if (dp[i + j])
-                    dp[i] = true;
-            }
+        int maxReached = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (maxReached < i) return false;
+            maxReached = Math.max(maxReached, i + nums[i]);
         }
         
-        return dp[0];
+        if (maxReached >= nums.length - 1)
+            return true;
+        return false;
     }
 }
