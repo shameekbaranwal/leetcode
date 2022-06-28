2119364277671c7a8b1dcbf144d9f3cd3893e7a6
Shameek <39854726+shameekbaranwal@users.noreply.github.com>
2022-06-28 15:28:02 +0530

    Time: 46 ms (16.82%), Space: 60.5 MB (21.81%) - LeetHub

--- 38-count-and-say/38-count-and-say.java
+++ 38-count-and-say/38-count-and-say.java
@@ -1,36 +1,20 @@
 class Solution {
-	public String countAndSay(int n) {
-		if (n == 1)
-			return "1";
-
-		String prev = countAndSay(n - 1);
-		return say(prev);
-	}
-
-	public String say(String s) {
-		int len = s.length();
-		StringBuilder result = new StringBuilder("");
-		int current = s.charAt(0) - '0', count = 1;
-
-		if (len == 1)
-			return (count + "" + current);
-
-		for (int i = 1; i < len; i++) {
-			int num = s.charAt(i) - '0';
-
-			if (num != current) {
-				result.append(count + "" + current);
-				current = num;
-				count = 0;
-			}
-			count++;
-
-			if (i == s.length() - 1) {
-				result.append(count + "" + current);
-				break;
-			}
-		}
-
-		return result.toString();
-	}
+    public String countAndSay(int n) {
+        return solve(n,"1");  
+    }
+    public String solve(int n, String s){
+        if(n==1) return s;
+        String res = "";
+        int count = 1;
+        for(int i=0;i<s.length();i++){
+            if((i+1)<s.length() && s.charAt(i)==s.charAt(i+1)){  // we are checking if the next character is same, if it is then we will increase the count 
+                count++;
+            }
+            else{
+                res+=count+""+s.charAt(i);  // if the next character is not the same then we will add the count then the character of which the count is, as the way if saying "11" is "two 1" i.e., "21" i.e., first the count then the character 
+                count = 1;  // after that character we again set our count to 1 for the next character
+            }
+        }
+        return solve(n-1,res);
+    }
 }
