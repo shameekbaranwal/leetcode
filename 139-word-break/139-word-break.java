51255e4dc990fbb40153aec43b6f03db2b81c48b
Shameek <39854726+shameekbaranwal@users.noreply.github.com>
2022-06-30 01:06:41 +0530

    Time: 4 ms (85.99%), Space: 42.6 MB (79.12%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,50 +1,22 @@
-import java.util.*;
-
 class Solution {
-	HashSet<String> set;
-	String s;
-	int len;
-	int dp[];
-
-	public boolean wordBreak(String s, List<String> wordDict) {
-		set = new HashSet<>(wordDict);
-		this.s = s;
-		this.len = s.length();
-		dp = new int[len];
-		return wordBreakRecursive(0);
-	}
-
-	public boolean wordBreakRecursive(int index) {
-		if (index == len)
-			return true;
-
-		if (dp[index] != 0)
-			return dp[index] == 1 ? true : false;
-
-		StringBuilder substr = new StringBuilder("");
-		for (int i = index; i < len; i++) {
-			substr.append(s.charAt(i));
-			if (set.contains(substr.toString())) {
-				if (wordBreakRecursive(i + 1)) {
-					dp[index] = 1;
-					return true;
-				}
-			}
-		}
-
-		dp[index] = -1;
-		return false;
-	}
-
-	public static void check(boolean actual, boolean expected, int tc) {
-		System.out.print("TEST CASE " + tc + "  - ");
-		System.out.println(expected == actual ? "PASSED (" + actual + ")"
-				: "FAILED\nExpected: " + expected + "\nActual: " + actual);
-	}
-
-	public static void main(String[] args) {
-		Solution obj = new Solution();
-		check(obj.wordBreak("applepenapple", List.of("apple", "pen")), true, 1);
-		check(obj.wordBreak("catsandog", List.of("cats", "dog", "sand", "and", "cat")), false, 2);
-	}
+    public boolean wordBreak(String s, List<String> dict) {
+        boolean[] found = new boolean[s.length()+1];
+        found[0] = true;
+        
+        for (int i = 0; i < s.length(); ++i) {
+            if (!found[i])
+                continue;
+            
+            for (String word : dict) {
+                int end = i + word.length();
+                if (end > s.length() || found[end])
+                    continue;
+                
+                if (s.substring(i, end).equals(word))
+                    found[end] = true;
+            }
+        }
+        
+        return found[s.length()];
+    }
 }
