b31790036d61342747858a494f3b082c2b32d861
Shameek <39854726+shameekbaranwal@users.noreply.github.com>
2022-06-30 16:54:34 +0530

    Time: 26 ms (5.24%), Space: 45.8 MB (5.37%) - LeetHub

---
+++ 120-triangle/120-triangle.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+	static List<List<Integer>> triangle;
+	static int rows;
+	static Map<String, Integer> map;
+
+	public static int minimumTotal(List<List<Integer>> tr) {
+		triangle = tr;
+		rows = tr.size();
+		map = new HashMap<>();
+		return minTot(0, 0);
+	}
+
+	public static String key(int row, int index) {
+		return row + "," + index;
+	}
+
+	public static int minTot(int row, int index) {
+		if (row == rows)
+			return 0;
+
+		if (map.containsKey(key(row, index)))
+			return map.get(key(row, index));
+
+		int s1 = minTot(row + 1, index);
+		int s2 = minTot(row + 1, index + 1);
+
+		map.put(key(row, index), triangle.get(row).get(index) + Math.min(s1, s2));
+		return map.get(key(row, index));
+	}
+
+	public static void assertEquals(
+
+			int actual,
+			int expected,
+			int tc) {
+		System.out.print("TEST CASE " + tc + "  - ");
+		System.out.println(expected == actual ? "PASSED (" + actual + ")"
+				: "FAILED\nExpected: " + expected + "\nActual: " + actual);
+	}
+
+	public static void main(String[] args) {
+		assertEquals(minimumTotal(List.of(List.of(2), List.of(3, 4), List.of(6, 5,
+				7),
+				List.of(4, 1, 8, 3))), 11, 1);
+		assertEquals(minimumTotal(List.of(List.of(-10))), -10, 2);
+	}
+}
