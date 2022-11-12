823944720b380b9a336e9a7a1dc00857f67ed072
Shameek <39854726+shameekbaranwal@users.noreply.github.com>
2022-11-13 02:44:19 +0530

    Time: 281 ms (5.73%), Space: 27.7 MB (8.76%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> map;
-        
+        unordered_map<string, vector<string>> m;
         for (int i = 0; i < strs.size(); i++) {
-            string count = getCount(strs[i]);
-            map[count].push_back(strs[i]);
+            string key = getKey(strs[i]);
+            m[key].push_back(strs[i]);
         }
         
         vector<vector<string>> result;
-        
-        for (auto pair : map) 
-            result.push_back(pair.second);
-        
+        for (auto it = m.begin(); it != m.end(); it++) {
+            result.push_back(it->second);
+        }
         return result;
     }
-    
-    string getCount(string str) {
-        vector<int> arr(26);
-        
-        for (int i = 0; i < str.length(); i++) 
-            arr[str[i] - 'a']++;
+private:
+    string getKey(string str) {
+        vector<int> count(26);
+        for (int j = 0; j < str.size(); j++) {
+            count[str[j] - 'a']++;
+        }
         
         string key = "";
-        for (int i = 0; i < 26; i++)
-            key.append(to_string(arr[i]) + " ");
-        
+        for (int i = 0; i < 26; i++) {
+            key.append(to_string(count[i] + 'a'));
+        }
         return key;
     }
 };
