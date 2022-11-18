d3d5aa016dff82eaf76d3ecd9234089177552fac
Shameek <39854726+shameekbaranwal@users.noreply.github.com>
2022-11-18 12:59:42 +0530

    Time: 568 ms (41.04%), Space: 46.4 MB (64.70%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        // make a hashset with the array
+        // every consecutive sequence in the array STARTS with a number, and we can find the starting element of all these sequences by looking for a number that doesn't have a corresponding number-1 element in the set.
+        // once we have a list of all the starting elements of all possible sequences, we just go through every one of them and count the number of consecutive elements present in the set.
+        
+        // make a hashset
+        unordered_set<int> set(nums.begin(), nums.end());
+        
+        vector<int> startOfSequences;
+        
+        // collect the starting number of every sequence
+        for (int i = 0; i < nums.size(); i++) {
+            if (set.find(nums[i] - 1) == set.end())
+                startOfSequences.push_back(nums[i]);
+        }
+        
+        // find the size of every sequence
+        int maxValue = 0;
+        for (int i = 0; i < startOfSequences.size(); i++) {
+            int value = startOfSequences[i];
+            int size = 0;
+            while (set.find(value) != set.end()) {
+                size++;
+                value++;
+            }
+            maxValue = max(maxValue, size);
+        }
+        
+        return maxValue;
+    }
+};
