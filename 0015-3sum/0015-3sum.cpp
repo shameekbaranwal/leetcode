aaa28c4e51687519ab04e81add91bf514e7008a1
Shameek <39854726+shameekbaranwal@users.noreply.github.com>
2022-11-21 13:58:43 +0530

    Time: 120 ms (81.15%), Space: 19.8 MB (85.25%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -7,7 +7,10 @@ public:
         if (nums.size() < 3) return triplets;
     
         for (int i = 0; i < nums.size(); i++) {
+            // no repeated elements in the triplet
+            // so first number shouldn't be a repeated occurrence
             if ((i > 0 && nums[i] == nums[i-1])) continue;
+            // numbers are sorted, so if reached a positive number, no more triplets exist
             if (nums[i] > 0) break;
                 
             int targetSum = -nums[i];
@@ -17,12 +20,14 @@ public:
             int end = nums.size() - 1;
             while (start < end) {
                 int sum = nums[start] + nums[end];
-                
+     
                 if (sum > targetSum) {
                     end--;
                     continue;
                 }
                 
+                // squashed two separate if conditions into one because they both do the same thing. first one is obvious, part of normal two sum. for the second condition:
+                // the SECOND number of the triplet can be the same as first occurrence (hence the `start > i+1`), but otherwise the second number can't be a repeated occurrence (hence the nums[start] == nums[start - 1])
                 if (sum < targetSum || 
                     (nums[start] == nums[start - 1] && start > i + 1)) {
                     start++;
