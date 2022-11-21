48978112cf01742e25f85c9319c3f880ca156f82
Shameek <39854726+shameekbaranwal@users.noreply.github.com>
2022-11-21 13:50:48 +0530

    Time: 74 ms (97.02%), Space: 19.8 MB (85.25%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> triplets;
+        
+        if (nums.size() < 3) return triplets;
+    
+        for (int i = 0; i < nums.size(); i++) {
+            if (isRepeated(nums, i)) continue;
+            if (nums[i] > 0) break;
+                
+            int targetSum = -nums[i];
+            
+            // two-pointer approach for two sum from i+1 to end.
+            int start = i + 1;
+            int end = nums.size() - 1;
+            while (start < end) {
+                int sum = nums[start] + nums[end];
+                
+                if (sum > targetSum) {
+                    end--;
+                    continue;
+                }
+                
+                if (sum < targetSum || (nums[start] == nums[start-1] && start > i + 1)) {
+                    start++;
+                    continue;
+                }
+                
+                triplets.push_back({ nums[i], nums[start], nums[end] });
+                start++;
+                end--;
+            }
+        }
+        
+        return triplets;
+    }
+    
+    bool isRepeated(vector<int> &nums, int i) {
+        return (i > 0 && nums[i] == nums[i-1]);
+    }
+};
