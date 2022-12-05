0e3fd97f7edceb7a4d0143d7ed210770c6be1c37
Shameek <39854726+shameekbaranwal@users.noreply.github.com>
2022-12-05 05:41:18 +0530

    Time: 32 ms (58.07%), Space: 8.9 MB (39.99%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,36 +3,47 @@ public:
     int lengthOfLongestSubstring(string s) {
         int i = 0, j = 1, len = s.size();
         if (len < 2) return len;
+        
         unordered_set<char> set;
         set.insert(s[0]);
         int max_length = 1;
+        
         bool travelling = true;
         char look_for;
         
         while (j < len || !travelling) {
-            // cout << s[i] << " " << s[j] << endl;
-            if (travelling) {
-                char c = s[j];
-                int sz = set.size();
-                set.insert(c);
-                if (set.size() == sz) {
-                    travelling = false;
-                    look_for = c;
+            if (!travelling) {
+                // since travelling is stopped because of a repeating char
+                // trace ahead with backward pointer to look for 
+                // the earlier occurrence of the repeating letter
+                char curr = s[i++];
+                // if this isn't the repeating letter, it's not in largest
+                // consecutive unique sequence anymore
+                if (look_for != curr) {
+                    set.erase(curr);
                     continue;
                 }
-                j++;
-                max_length = max(max_length, (int) set.size());
-                continue;
-            }
-            char curr = s[i++];
-            // cout << "hello" << endl;
-            
-            if (look_for == curr) {
+                // when first occurrence found, start travelling again.
                 j++;
                 travelling = true;
+                continue;
             }
-            else
-                set.erase(curr);
+            // make the forward pointer travel ahead
+            // as long as consecutive letters are unique
+            char c = s[j];
+            int sz = set.size();
+            set.insert(c);
+            // if the letter was not unique, stop travelling,
+            if (set.size() == sz) {
+                travelling = false;
+                look_for = c;
+                continue;
+            }
+            // if letter was unique, keep track of max length and
+            // keep travelling ahead
+            j++;
+            max_length = max(max_length, (int) set.size());
+            continue;
             
         }
         
